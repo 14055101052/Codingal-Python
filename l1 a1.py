cb7d296e5190df18e770fe68efe2401a74a9f0ab
14055101052 <113033228+14055101052@users.noreply.github.com>
2026-03-30 23:33:36 +0530

    Add files via upload

---
+++ l1 a1.py
@@ -0,0 +1,8 @@
+print("""
+Hello Everyone 
+To The World Of Python
+I will INTROUCE myself """)
+name = "Mehan Jain"
+age = 16
+hobby ="Playing cricket and football"
+print(f"My name is {name}.\nI am {age} years old.\nMy hobby is {hobby}")
