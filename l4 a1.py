a795f2e59b89305b07a02058a4e47aff238f6932
14055101052 <113033228+14055101052@users.noreply.github.com>
2026-04-06 17:51:27 +0530

    Add files via upload

---
+++ l4 a1.py
@@ -0,0 +1,11 @@
+tree1 = 45
+tree2 = 32
+tree3 = 12
+tree4 = 65
+tree5 = 50
+
+sum = tree1+tree2+tree3+tree4+tree5
+print("The su of 5 trees is : ", sum)
+
+average = sum/5
+print("The average of 5 trees is : ", average)
