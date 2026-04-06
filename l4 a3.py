a7ff5ce5ca6dc82fc581c3972605050884523625
14055101052 <113033228+14055101052@users.noreply.github.com>
2026-04-06 18:14:06 +0530

    Add files via upload

---
+++ l4 a3.py
@@ -0,0 +1,10 @@
+maths = int(input("Please enter your marks : "))
+english = int(input("Please enter your marks : "))
+hindi = int(input("Please enter your marks : "))
+science = int(input("Please enter your marks : "))
+
+sum = maths+science+english+hindi
+print("The sum of marks : ", sum)
+
+percentage = (sum/400)*100
+print("The total percentage obtained is : ", percentage)
