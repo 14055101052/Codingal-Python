5230fb7fdbd37ac0fd7301a26edd88511c21a652
14055101052 <113033228+14055101052@users.noreply.github.com>
2026-04-08 18:06:55 +0530

    Add files via upload

---
+++ l6 a3.py
@@ -0,0 +1,25 @@
+a = 10
+b = 12
+c = 12
+
+print(a != b)
+print(b != c)
+
+a = "python"
+b = "coding"
+
+if a != b :
+    print("Hello")
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+    print("Python")
+
+num = int(input("Enter a number : "))
+
+if num%2 == 0:
+    print(num, "is an even number")
+else : 
+    print(num, "is not an even number")
