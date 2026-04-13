f62109e90dd0464f0660f9bf8b073851b4fb686b
14055101052 <113033228+14055101052@users.noreply.github.com>
2026-04-13 18:13:08 +0530

    Add files via upload

---
+++ l7 a1.py
@@ -0,0 +1,21 @@
+x = 5
+if (type(x) is int) :
+    print("True")
+else :
+    print("False")
+
+x = 5.5
+if (type(x) is not float) : 
+    print("True")
+else :
+    print("False")
+
+x = 20
+y = 20
+
+if (x is y):
+    print("x & y have same identity")
+
+y = 30
+if (x is not y):
+    print("x & y have different identity")
---
+++ l7 a2.py
@@ -0,0 +1,28 @@
+# a = 5
+# b = 4
+
+# print("a >> 1 : ", a>>1)
+# print("b >> 1 : ", b>>1)
+
+# a = 4
+# b = 5
+
+# print("a << 1 : ", a<<1)
+# print("b << 1 : ", b<<1)
+
+# identity operator
+# x = [1,2]
+# y = [1,2]
+# z = x
+
+# print(f"id of x : ", id(x))
+# print(f"id of y : ", id(y))
+# print(f"id of z : ", id(z))
+
+# membership operator
+email = "mehan100@gmail.com"
+
+print("mehan" in  email)
+print("100" in  email)
+print("100" not in  email)
+print("gmail" in  email)
